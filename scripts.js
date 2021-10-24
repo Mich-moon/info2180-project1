f04dbbf8ad3354b78993e83daa992ef61b3123bc
Michaela Wright <75323745+Mich-moon@users.noreply.github.com>
2021-10-24 03:30:05 -0500

    javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,38 @@
 /* Add your JavaScript to this file */
+"use strict";
+
+window.onload = function() {
+
+  var button = document.getElementsByTagName("button")[0];
+
+  // click event for the newsletter button
+  button.addEventListener("click", function (element) {
+    element.preventDefault();
+
+    var email = document.querySelector('#email');   
+    var message = document.querySelector('.message');
+
+    // remove message from previous button click
+    if(message.firstChild){
+        message.removeChild(message.lastChild);
+    }
+
+    if ( email.value.trim().length == 0) {  
+        // display error message
+        var errorMessageText = document.createTextNode("Please enter a valid email address.");
+        var errorMessage = document.createElement("span");
+        errorMessage.appendChild(errorMessageText);
+        message.appendChild(errorMessage);
+    } else {
+        // display email message
+        var mail = email.value;
+        var messageText = document.createTextNode("Thank you! Your email address " +mail+" has been added to out mailing list!");
+        var messageBox = document.createElement("span");
+        messageBox.appendChild(messageText);
+        message.appendChild(messageBox);
+    }
+
+  });
+
+
+};
